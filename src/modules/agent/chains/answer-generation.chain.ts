547cb6a89c5725d92f953d6844a693c4688ffec1
denis.shemenko <denis.shemenko@idealscorp.com>
2024-04-17 00:23:55 +0400

    Add existing files to Git

---
+++ src/modules/agent/chains/answer-generation.chain.ts
@@ -0,0 +1,52 @@
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { PromptTemplate } from "@langchain/core/prompts";
+import { RunnableSequence } from "@langchain/core/runnables";
+import { OpenAI } from "@langchain/openai";
+import { BaseLanguageModel } from "langchain/base_language";
+
+// tag::interface[]
+export interface GenerateAnswerInput {
+  question: string;
+  context: string;
+}
+// end::interface[]
+
+// tag::function[]
+export default function initGenerateAnswerChain(
+  llm: BaseLanguageModel
+): RunnableSequence<GenerateAnswerInput, string> {
+  const answerQuestionPrompt = PromptTemplate.fromTemplate(`
+    Use only the following context to answer the following question.
+
+    Question:
+    {question}
+
+    Context:
+    {context}
+
+    Answer as if you have been asked the original question.
+    Do not use your pre-trained knowledge.
+
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Include links and sources where possible.`
+  );
+
+  return RunnableSequence.from<GenerateAnswerInput, string>([
+    answerQuestionPrompt, 
+    llm, 
+    new StringOutputParser()
+  ]);
+}
+// end::function[]
+
+/*
+// tag::usage[]
+const llm = new OpenAI() // Or the LLM of your choice
+const answerChain = initGenerateAnswerChain(llm)
+
+const output = await answerChain.invoke({
+  question: 'Who is the CEO of Neo4j?',
+  context: 'Neo4j CEO: Emil Eifrem',
+}) // Emil Eifrem is the CEO of Neo4j
+// end::usage[]
+*/
